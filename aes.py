1863f18ada7f89b3e41e2488424bf8ed1892e0be
LiamCrsd <43929554+LiamCrsd@users.noreply.github.com>
2021-02-03 23:51:01 +0100

    Add files via upload

---
+++ aes.py
@@ -0,0 +1,19 @@
+import aes_dec as dec
+import aes_enc as enc
+from convert import *
+
+def encrypt(text, key):
+
+    mat_text = ascii_2_matrix(text)
+    mat_key = key_2_matrix(key)
+    res = enc.AES(mat_text, mat_key)
+    return res
+
+def decrypt(text, key):
+    mat_key = key_2_matrix(key)
+    if type(text) == str:
+        mat_text = ascii_2_matrix(text)
+    else:
+        mat_text = text
+    res = dec.InvAES(mat_text,mat_key)
+    return matrix_2_ascii(res)
